566af3f5c64d1c05a8442b6ab361afd135d63a40
bmerrill2 <bradmerrill@eighty1designs.com>
2015-10-22 00:03:50 -0600

    Finished the Mini-Project

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,9 @@
+app.controller('productsCtrl', function ($scope, $routeParams, productService) {
+
+  if ($routeParams.id === 'shoes') {
+    $scope.productData = productService.shoeData;
+  } else if ($routeParams.id === 'socks') {
+    $scope.productData = productService.sockData;
+  }
+
+});
